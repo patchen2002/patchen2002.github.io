2421994ef1cfcb2b93ce7bd67ba708a6ce0ab52a
Patrick Chen <patrickchen@Patricks-MacBook-Pro.local>
2023-04-13 10:19:07 -0400

    Added responsiveness and resume

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import LinkedInLogo from "./assets/linkedin_png.png" 
 import GitHubLogo from "./assets/github_png.png" 
 import EmailLogo from "./assets/email_png.png" 
+import ResumeLogo from "./assets/resume_png.png" 
 import HeadShot from "./assets/headshot_circle.png"
 import StablerLogo from "./assets/stabler_logo.png"
 import RiskLogo from "./assets/risk_logo.png"
@@ -13,39 +14,44 @@ function App() {
     <div>
       <div className="flex justify-center text-center items-center h-screen bg-plat">
         <div>
-          <p className="text-5xl font-bold text-redw fade-in">CS @ Cornell University</p>
-          <p className="text-9xl font-bold text-coffee p-8 fade-in">PATRICK CHEN</p>
-          <p className="text-5xl font-bold text-redw fade-in">Software Engineer</p>
+          <p className="sm:text-5xl font-bold text-redw fade-in text-3xl">CS @ Cornell University</p>
+          <p className="sm:text-9xl font-bold text-coffee p-8 fade-in xs:text-7xl text-6xl">PATRICK CHEN</p>
+          <p className="sm:text-5xl font-bold text-redw fade-in text-3xl">Software Engineer</p>
           <div className="flex justify-center">
             <a href="https://www.linkedin.com/in/patrick-chen-34b9b1205/" target="_blank" rel="noreferrer"><img
-            className="transition ease-in-out duration-700 opacity-40 h-16 w-16 m-8 fade-in-image hover:opacity-100" 
+            className="transition ease-in-out duration-700 opacity-40 xs:h-16 xs:w-16 xs:m-8 fade-in-image hover:opacity-100 h-12 w-12 mx-4 my-6" 
             src={LinkedInLogo}
             alt="LinkedIn Logo"
             ></img></a>
             <a href="https://github.com/patchen2002" target="_blank" rel="noreferrer"><img
-            className="transition ease-in-out duration-700 opacity-40 h-16 w-16 m-8 fade-in-image hover:opacity-100" 
+            className="transition ease-in-out duration-700 opacity-40 xs:h-16 xs:w-16 xs:m-8 fade-in-image hover:opacity-100 h-12 w-12 mx-4 my-6" 
             src={GitHubLogo}
             alt="GitHub Logo"
             ></img></a>
             <a href="mailto:pchen2002@gmail.com"><img
-            className="transition ease-in-out duration-700 opacity-40 h-16 w-16 m-8 fade-in-image hover:opacity-100" 
+            className="transition ease-in-out duration-700 opacity-40 xs:h-16 xs:w-16 xs:m-8 fade-in-image hover:opacity-100 h-12 w-12 mx-4 my-6" 
             src={EmailLogo}
             alt="Email Logo"
+            ></img></a>
+            <a href="/assets/patrick_resume.pdf" target="_blank" rel="noreferrer"><img
+            className="transition ease-in-out duration-700 opacity-40 xs:h-16 xs:w-16 xs:m-8 fade-in-image hover:opacity-100 h-12 w-12 mx-4 my-6" 
+            src={ResumeLogo}
+            alt="Resume Logo"
             ></img></a>
           </div>
         </div>
       </div>
-      <div className="flex justify-center text-center items-center h-screen border-b border-plat">
+      <div className="flex justify-center text-center items-center md:h-screen border-b border-plat">
         <div>
-          <p className="text-2xl text-redw">About Me</p>
-          <p className="text-7xl font-bold text-coffee p-5">A Quick Introduction</p>
+          <p className="text-2xl text-redw my-6">About Me</p>
+          <p className="sm:text-7xl font-bold text-coffee pb-5 text-5xl">A Quick Introduction</p>
           <img
             className="h-44 w-44 mx-auto my-4" 
             src={HeadShot}
             alt="Headshot"
             ></img>
             <div>
-              <p className="text-charc text-xl my-6 mx-16">My name is Patrick Chen, and welcome to my portfolio! I am currently a rising senior at Cornell Univeristy pursuing a major in 
+              <p className="text-charc text-xl my-6 med:mx-32 xs:mx-16 mx-8">My name is Patrick Chen, and welcome to my portfolio! I am currently a rising senior at Cornell University pursuing a major in 
               CS and a minor in Math. I am interested in many different areas of software engineering, specifically full stack development, machine learning, and algorithm development. When I am not 
               coding, I love to weightlift, cook, and eat what I cook. </p>
             </div>
@@ -54,15 +60,15 @@ function App() {
       <div className="flex justify-center text-center">
         <div className="py-24 border-b border-plat w-screen">
           <p className="text-2xl text-redw">The Important Stuff</p>
-          <p className="text-7xl font-bold text-coffee p-5">Work Experience</p>
+          <p className="sm:text-7xl font-bold text-coffee p-5 text-5xl">Work Experience</p>
             <div>
-              <p className="text-charc text-6xl my-8">Capital One</p>
+              <p className="text-charc sm:text-6xl my-8 text-4xl">Capital One</p>
               <p className="text-charc text-xl">Software Engineer Intern</p>
               <p className="text-charc text-xl">June 2023-August 2023</p>
-              <p className="text-charc text-6xl my-8">Pegasystems</p>
+              <p className="text-charc sm:text-6xl my-8 text-4xl">Pegasystems</p>
               <p className="text-charc text-xl">Software Engineer Intern</p>
               <p className="text-charc text-xl">May 2022-August 2022</p>
-              <p className="text-charc text-6xl my-8">U4Ea</p>
+              <p className="text-charc sm:text-6xl my-8 text-4xl">U4Ea</p>
               <p className="text-charc text-xl">Software Engineer Intern</p>
               <p className="text-charc text-xl">December 2021-January 2022</p>
             </div>
@@ -73,15 +79,15 @@ function App() {
           <p className="text-2xl text-redw">Some Cool Things I Made</p>
           <p className="text-7xl font-bold text-coffee p-5">Projects</p>
             <div>
-              <div className="flex items-center justify-center h-full mx-auto">
+              <div className="flex items-center justify-center h-full mx-auto sm:my-0 xs:my-4 my-8">
                 <div>
                   <a href="https://github.com/erichuang27/stabler_coin" target="_blank" rel="noreferrer"><img
-                  className="transition ease-in-out duration-500 h-44 w-44 mx-12 my-6 hover:opacity-60" 
+                  className="transition ease-in-out duration-500 xs:h-44 xs:w-44 m-6 hover:opacity-60 w-28 h-28" 
                   src={StablerLogo}
                   alt="Stabler Coin Logo"
                   ></img></a>
                 </div>
-                <div className="w-1/2">
+                <div className="xs:w-1/2 mx-6 w-3/4">
                     <p className="text-3xl font-bold text-charc">Stabler Coin</p>
                     <p className="text-charc">May 2022</p>
                     <p className="text-charc">Stabler Coin is an over-collateralized, decentralized, and diversified stablecoin. 
@@ -90,15 +96,15 @@ function App() {
                     </p>
                 </div>
               </div>
-              <div className="flex items-center justify-center h-full mx-auto">
+              <div className="flex items-center justify-center h-full mx-auto sm:my-0 xs:my-4 my-8">
                 <div>
                   <img
-                  className="h-44 w-44 mx-12 my-6" 
+                  className="transition ease-in-out duration-500 xs:h-44 xs:w-44 m-6 hover:opacity-60 w-28 h-28" 
                   src={RiskLogo}
                   alt="Risk Logo"
                   ></img>
                 </div>
-                <div className="w-1/2">
+                <div className="xs:w-1/2 mx-6 w-3/4">
                     <p className="text-3xl font-bold text-charc">Risk</p>
                     <p className="text-charc">May 2022</p>
                     <p className="text-charc">This is a terminal line version of the board game Risk coded in OCaml. 
@@ -106,15 +112,15 @@ function App() {
                     </p>
                 </div>
               </div>
-              <div className="flex items-center justify-center h-full mx-auto">
+              <div className="flex items-center justify-center h-full mx-auto sm:my-0 xs:my-4 my-8">
                 <div>
                   <a href="https://github.com/patchen2002/My-Journal" target="_blank" rel="noreferrer"><img
-                  className="transition ease-in-out duration-500 h-44 w-44 mx-12 my-6 hover:opacity-60" 
+                  className="transition ease-in-out duration-500 xs:h-44 xs:w-44 m-6 hover:opacity-60 w-28 h-28" 
                   src={MyJournalLogo}
                   alt="My Journal Logo"
                   ></img></a>
                 </div>
-                <div className="w-1/2">
+                <div className="xs:w-1/2 mx-6 w-3/4">
                     <p className="text-3xl font-bold text-charc">My Journal</p>
                     <p className="text-charc">April 2022</p>
                     <p className="text-charc">My Journal is a web application that allows users to create their own journal entries. 
@@ -123,15 +129,15 @@ function App() {
                     </p>
                 </div>
               </div>
-              <div className="flex items-center justify-center h-full mx-auto">
+              <div className="flex items-center justify-center h-full mx-auto sm:my-0 xs:my-4 my-8">
                 <div>
                   <a href="https://github.com/connorjchen/BigRedTalks" target="_blank" rel="noreferrer"><img
-                  className="transition ease-in-out duration-500 h-44 w-44 mx-12 my-6 hover:opacity-60" 
+                  className="transition ease-in-out duration-500 xs:h-44 xs:w-44 m-6 hover:opacity-60 w-28 h-28" 
                   src={BigRedTalksLogo}
                   alt="Big Red Talks Logo"
                   ></img></a>
                 </div>
-                <div className="w-1/2">
+                <div className="xs:w-1/2 mx-6 w-3/4">
                     <p className="text-3xl font-bold text-charc">Big Red Talks</p>
                     <p className="text-charc">January 2022</p>
                     <p className="text-charc">BigRedTalks is a live chatroom that works only for verified Cornell students who are on campus. 
@@ -140,15 +146,15 @@ function App() {
                     </p>
                 </div>
               </div>
-              <div className="flex items-center justify-center h-full mx-auto">
+              <div className="flex items-center justify-center h-full mx-auto sm:my-0 xs:my-4 my-8">
                 <div>
                   <a href="https://github.com/patchen2002/Synonym-Finder" target="_blank" rel="noreferrer"><img
-                  className="transition ease-in-out duration-500 h-44 w-44 mx-12 my-6 hover:opacity-60" 
+                  className="transition ease-in-out duration-500 xs:h-44 xs:w-44 m-6 hover:opacity-60 w-28 h-28" 
                   src={SynonymFinderLogo}
                   alt="Synonym Finder Logo"
                   ></img></a>
                 </div>
-                <div className="w-1/2">
+                <div className="xs:w-1/2 mx-6 w-3/4">
                     <p className="text-3xl font-bold text-charc">Synonym Finder</p>
                     <p className="text-charc">December 2021</p>
                     <p className="text-charc">Synonym Finder is a Python program where users can input multiple words/phrases and it will generate a list of synonyms. 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
     colors: {
       'plat': '#DCE0D9',
       'charc': '#28536B',
